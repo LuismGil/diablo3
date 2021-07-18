5f05c4625b35998a9cacf5ec48169180b0d9a2a0
LuismGil <luism6977@gmail.com>
2021-07-18 14:51:46 -0300

    Instaled libraries

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
+import { BootstrapVue } from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// import './plugins/bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
